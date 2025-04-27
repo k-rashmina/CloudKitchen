2efdf139ffbb7783f636fa144109f4228119f10e
Christmith <it2232434@my.sliit.lk>
2025-04-28 01:09:05 +0530

    Uncomment route for creating checkout session and update path to root

--- payment-service/src/routes/index.js
+++ payment-service/src/routes/index.js
@@ -3,6 +3,7 @@ const { createCheckoutSession } = require("../controllers/paymentController");
 
 const router = express.Router();
 
-router.post("/payment/create-checkout-session", createCheckoutSession);
+// router.post("/payment/create-checkout-session", createCheckoutSession);
+router.post("/create-checkout-session", createCheckoutSession);
 
 module.exports = router;
