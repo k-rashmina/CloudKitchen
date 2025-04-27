37805ba6e97443d08aff29c0b3ee0ef43475f61e
Christmith <it2232434@my.sliit.lk>
2025-04-27 22:02:11 +0530

    Add POST route for creating Stripe checkout session

--- payment-service/src/routes/index.js
+++ payment-service/src/routes/index.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const { createCheckoutSession } = require("../controllers/paymentController");
+
 const router = express.Router();
+
+router.post("/payment/create-checkout-session", createCheckoutSession);
 
 module.exports = router;
